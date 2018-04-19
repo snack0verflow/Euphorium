a97c91d5d3af28c0d3071c80f2bf21d37ebdba65
ConstipatedUnicorn <ConstipatedUnicorn@users.noreply.github.com>
2018-04-19 18:21:52 +0530

    first commit

---
+++ src/pms/SqlConn.java
@@ -0,0 +1,22 @@
+package pms;
+
+import java.sql.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
+public class SqlConn
+{
+	Connection con=null;
+	public static Connection dbCon()
+	{
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost/straffic?useSSL=false&"+"user=system&password=manager");
+			//JOptionPane.showMessageDialog(null, "connected");
+			return con;
+		}
+		catch(Exception e) {e.printStackTrace();
+		return null;}
+	}
+}
